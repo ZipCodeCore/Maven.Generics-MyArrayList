e1b0dcaa7feefe0645e1bdb629eed269cb2f8057
Mitch Taylor <taylor.mitch.5@gmail.com>
2018-03-08 15:12:05 -0500

    Part 1 stubbed and prepped

---
+++ src/main/java/MyArrayList.java
@@ -0,0 +1,33 @@
+public class MyArrayList<E> {
+
+    private E[] theArray;
+
+    public void add(E thingToAdd) {
+
+    }
+
+    public E[] getTheArray() {
+        return theArray;
+    }
+
+    public void remove(E thingToRemove) {
+
+    }
+
+    public void setTheArray(E[] theArray) {
+        this.theArray = theArray;
+    }
+
+    public void clear() {
+
+    }
+
+    public boolean isEmpty() {
+        return false;
+    }
+
+    public boolean contains(E thingToCheckFor) {
+        return false;
+    }
+
+}
---
+++ src/test/java/MyArrayListTest.java
@@ -0,0 +1,40 @@
+import org.junit.Test;
+
+public class MyArrayListTest {
+
+    @Test
+    public void setTheArrayTest() {
+
+    }
+
+    @Test
+    public void getTheArrayTest() {
+
+    }
+
+    @Test
+    public void addTest() {
+
+    }
+
+    @Test
+    public void removeTest() {
+
+    }
+
+    @Test
+    public void clearTest() {
+
+    }
+
+    @Test
+    public void isEmptyTest() {
+
+    }
+
+    @Test
+    public void containsTest() {
+
+    }
+
+}
