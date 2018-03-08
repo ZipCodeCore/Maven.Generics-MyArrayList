25e710a4f113536db441ab1b50568be1dd95e3be
Brian He <brianhe@zipcoders-MacBook-Pro-56.local>
2018-03-08 15:55:48 -0500

    finish

--- src/main/java/MyArrayList.java
+++ src/main/java/MyArrayList.java
@@ -1,2 +1,75 @@
-public class MyArrayList {
+import java.util.Arrays;
+
+public class MyArrayList <E> {
+
+    protected E[] inputArray;
+    protected E value;
+
+    public MyArrayList() {
+        inputArray = (E[]) new Object[0];
+    }
+
+    public MyArrayList(int size) {
+        inputArray = (E[]) new Object[size];
+    }
+
+    public void add(E valueToAdd) {
+        E[] newArray = Arrays.copyOf(inputArray, inputArray.length + 1);
+        newArray[newArray.length - 1] = valueToAdd;
+        this.inputArray = newArray;
+    }
+
+    public void add(E valueToAdd, Integer index) {
+        E[] newArray = Arrays.copyOf(inputArray, inputArray.length + 1);
+        for(int i = 0, j = 0; i < newArray.length; i++) {
+            if (i != index) {
+                newArray[i] = inputArray[j];
+                j++;
+            } else {
+                newArray[index] = valueToAdd;
+            }
+            this.inputArray = newArray;
+        }
+    }
+
+    public E get(Integer index) {
+        this.value = this.inputArray[index];
+        return this.value;
+    }
+
+    public void remove(Integer index) {
+        E[] newArray = Arrays.copyOf(inputArray, inputArray.length - 1);
+
+        for(int i = 0, j = 0; i < this.inputArray.length; i++) {
+            if(i != index) {
+                newArray[j] = this.inputArray[i];
+                j++;
+            }
+        }
+        this.inputArray = newArray;
+    }
+
+    public void set(E valueToSet, Integer index) {
+        this.inputArray[index] = valueToSet;
+    }
+
+    public void clear() {
+        this.inputArray = (E[]) new Object[0];
+    }
+
+    public boolean isEmpty() {
+        if(inputArray.length == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean contains(E valueToCheck) {
+        for(int i = 0; i < this.inputArray.length; i++) {
+            if(inputArray[i] == valueToCheck) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
--- src/test/java/MyArrayListTest.java
+++ src/test/java/MyArrayListTest.java
@@ -1,2 +1,87 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class MyArrayListTest {
+
+    @Test
+    public void addIntegerTest() {
+        MyArrayList<Integer> myArrayList = new MyArrayList<>();
+        Integer expected = 5;
+        myArrayList.add(expected);
+
+        Integer actual = myArrayList.get(0);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void addStringTest() {
+        MyArrayList<String> myArrayList = new MyArrayList<>();
+        String expected = "Hi";
+        myArrayList.add(expected);
+
+        String actual = myArrayList.get(0);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void addWithIndexTest() {
+        MyArrayList<Integer> myArrayList = new MyArrayList<>();
+        myArrayList.add(5);
+        myArrayList.add(7);
+        Integer expected = 6;
+
+        myArrayList.add(expected, 1);
+        Integer actual = myArrayList.get(1);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void removeTest() {
+        MyArrayList<Integer> myArrayList = new MyArrayList<>();
+        myArrayList.add(5);
+        myArrayList.add(6);
+
+        myArrayList.remove(0);
+        Integer expected = 6;
+        Integer actual = myArrayList.get(0);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setTest() {
+        MyArrayList<Integer> myArrayList = new MyArrayList<>();
+        myArrayList.add(5);
+        myArrayList.add(6);
+
+        myArrayList.set(200, 1);
+
+        Integer expected = 200;
+        Integer actual = myArrayList.get(1);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void clearTest() {
+        MyArrayList<Integer> myArrayList = new MyArrayList<>();
+        myArrayList.add(5);
+        myArrayList.add(6);
+
+        myArrayList.clear();
+
+        Assert.assertTrue(myArrayList.isEmpty());
+
+    }
+
+    @Test
+    public void containsTest() {
+        MyArrayList<Integer> myArrayList = new MyArrayList<>();
+        myArrayList.add(5);
+        myArrayList.add(6);
+
+        Assert.assertTrue(myArrayList.contains(6));
+    }
 }
