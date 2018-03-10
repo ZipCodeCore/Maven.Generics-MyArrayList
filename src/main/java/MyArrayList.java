91b28d6e1650522924e7dc945b3ce38f37945086
Luis Romero <luisromero@zipcoders-MacBook-Pro-112.local>
2018-03-10 17:35:01 -0500

    finished methods/tests for add, get, set, ensurecapacity

--- src/main/java/MyArrayList.java
+++ src/main/java/MyArrayList.java
@@ -1,27 +1,40 @@
+import java.util.Arrays;
+
 public class MyArrayList<E> {
 
     private E[] myArrayList;
-    private int size;
+    private int size = 0;
 
     public MyArrayList() {
         this.myArrayList = (E[]) new Object[0];
     }
 
-    public MyArrayList(int size) {
-        this.myArrayList = (E[]) new Object[size];
+    public MyArrayList(int length) {
+        this.myArrayList = (E[]) new Object[length];
+    }
+
+    public E[] getMyArrayList() {
+        return myArrayList;
     }
 
     public boolean add(E element) {
-        // adds element to the end of the array
-        // returns true;
-        return false;
+        ensureCapacity();
+        myArrayList[size] = element;
+        size++;
+        return true;
     }
 
     public void add(int index, E element) {
+        if (index < 0) {
+            throw new ArrayIndexOutOfBoundsException(index);
+        }
+        ensureCapacity();
+        myArrayList[index] = element;
+        size++;
     }
 
-    public E get(){
-        return null;
+    public E get(int index){
+        return myArrayList[index];
     }
 
     public E remove(int index) {
@@ -31,6 +44,12 @@ public class MyArrayList<E> {
 
     public E set(int index, E element) {
         // returns the element previously at the specified position
+        E previousElement = myArrayList[index];
+        if (index < 0 || index >= myArrayList.length) {
+            throw new ArrayIndexOutOfBoundsException(index);
+        }
+        myArrayList[index] = element;
+        return previousElement;
     }
 
     public void clear() {
@@ -44,5 +63,18 @@ public class MyArrayList<E> {
     public boolean contains() {
         return false;
     }
+
+    public int length() {
+        return myArrayList.length;
+    }
+
+    public void ensureCapacity() {
+        if (size == myArrayList.length) {
+            E[] arrayWithDoubleCapacity = (E[]) new Object[size * 2];
+            System.arraycopy(myArrayList, 0, arrayWithDoubleCapacity, 0, size);
+            myArrayList = arrayWithDoubleCapacity;
+        }
+    }
+
 
 }
--- src/test/java/MyArrayListTest.java
+++ src/test/java/MyArrayListTest.java
@@ -3,8 +3,89 @@ import org.junit.Assert;
 
 public class MyArrayListTest {
 
-    @Test
-    public void name() {
+    private MyArrayList<Integer> mal;
 
+    @Test
+    public void MyArrayListConstructorLengthTest() {
+        // Given
+        int expectedArrayLength = 1;
+        mal = new MyArrayList<>(1);
+        // When
+        int actualArrayLength = mal.length();
+        // Then
+        Assert.assertEquals(expectedArrayLength, actualArrayLength);
     }
+
+    @Test
+    public void addElementTest() {
+        // Given
+        int expectedIntAtIndex0 = 0;
+        int expectedIntAtIndex1 = 1;
+        int startingLength = 1;
+//        int expectedIntToRemove = 0;
+        // When
+        mal = new MyArrayList<>(startingLength);
+        mal.add(expectedIntAtIndex0);
+        mal.add(expectedIntAtIndex1);
+        int actualIntAtIndex1 = mal.get(1);
+        // Then
+        Assert.assertEquals(expectedIntAtIndex1, actualIntAtIndex1);
+    }
+
+    @Test
+    public void addIndexElementTest() {
+        // Given
+        int expectedIndex0 = 0;
+        int expectedIntAtIndex0 = 0;
+        int expectedIndex1 = 1;
+        int expectedIntAtIndex1 = 1;
+        int startingLength = 1;
+//        int expectedIntToRemove = 0;
+        // When
+        mal = new MyArrayList<>(startingLength);
+        mal.add(expectedIndex0, expectedIntAtIndex0);
+        mal.add(expectedIndex1, expectedIntAtIndex1);
+        int actualIntAtIndex1 = mal.get(1);
+        // Then
+        Assert.assertEquals(expectedIntAtIndex1, actualIntAtIndex1);
+    }
+
+    @Test
+    public void getTest() {
+        // Given
+        int expectedArrayLength = 1;
+        mal = new MyArrayList<>(1);
+        // When
+        int actualArrayLength = mal.length();
+        // Then
+        Assert.assertEquals(expectedArrayLength, actualArrayLength);
+    }
+
+    @Test
+    public void setTest() {
+        // Given
+        int expectedArrayLength = 1;
+        int expectedInteger = 7;
+        mal = new MyArrayList<>(1);
+        // When
+        mal.set(0, expectedInteger);
+        int actualInteger = mal.get(0);
+        // Then
+        Assert.assertEquals(expectedInteger, actualInteger);
+    }
+
+    @Test
+    public void ensureCapacityTest() {
+        // Given
+        int expectedArrayLength = 2;
+        int startingArrayLength = 1;
+        mal = new MyArrayList<>(startingArrayLength);
+        // When
+        mal.add(7);
+        mal.add(8);
+        int actualArrayLength = mal.length();
+        // Then
+        Assert.assertEquals(expectedArrayLength, actualArrayLength);
+    }
+
 }
