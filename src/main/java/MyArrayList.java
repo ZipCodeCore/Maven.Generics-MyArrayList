d221a16622af8aadc30c13895db486c327a4db37
Luis Romero <luisromero@zipcoders-MacBook-Pro-112.local>
2018-03-10 15:27:36 -0500

    adding methods' names

--- src/main/java/MyArrayList.java
+++ src/main/java/MyArrayList.java
@@ -1,9 +1,48 @@
-public class MyArrayList<T> {
+public class MyArrayList<E> {
 
-    private T[] myArrayList;
+    private E[] myArrayList;
     private int size;
 
     public MyArrayList() {
+        this.myArrayList = (E[]) new Object[0];
+    }
+
+    public MyArrayList(int size) {
+        this.myArrayList = (E[]) new Object[size];
+    }
+
+    public boolean add(E element) {
+        // adds element to the end of the array
+        // returns true;
+        return false;
+    }
+
+    public void add(int index, E element) {
+    }
+
+    public E get(){
+        return null;
+    }
+
+    public E remove(int index) {
+        // return the element that was removed
+        return null;
+    }
+
+    public E set(int index, E element) {
+        // returns the element previously at the specified position
+    }
+
+    public void clear() {
 
     }
+
+    public boolean isEmpty() {
+        return false;
+    }
+
+    public boolean contains() {
+        return false;
+    }
+
 }
