071b5df7856086feab566247894841b7be976c4e
Carolynn Vansant <cvansant@MacBook-Pro-5.fios-router.home>
2018-03-09 23:59:46 -0500

    completed MyArrayList and MySet

--- src/main/java/MySet.java
+++ src/main/java/MySet.java
@@ -6,6 +6,14 @@ public class MySet <T> {
 
     private MyArrayList<T> mySet;
 
+    public MySet() {
+        this.mySet = new MyArrayList<>(16);
+    }
+
+    public MySet(int size) {
+        this.mySet = new MyArrayList<>(size);
+    }
+
     public MySet(T[] setInput) {
        this.mySet = new MyArrayList<>(setInput);
        for(int i = 0; i<mySet.size(); i++){
@@ -15,11 +23,32 @@ public class MySet <T> {
        }
     }
 
-    public MyArrayList getMySet() {
-        return mySet;
+    public T[] getMySet() {
+        return mySet.getMyArrayList();
     }
 
     public void add(T value){
-        mySet.add(value);
+        if(!mySet.contains(value)) mySet.add(value);
     }
+
+    public void remove(int index){
+        mySet.remove(index);
+    }
+
+    public int size(){
+        return mySet.size();
+    }
+
+    public boolean contains(T value){
+        return mySet.contains(value);
+    }
+
+    public boolean isEmpty(){
+        return mySet.isEmpty();
+    }
+
+    public void clear(){
+        mySet.clear();
+    }
+
 }
--- src/test/java/MySetTest.java
+++ src/test/java/MySetTest.java
@@ -8,19 +8,93 @@ public class MySetTest {
     public void constructorTest(){
         String[] input = {"a", "b", "c", "a", "d", "a"};
         MySet<String> mySet = new MySet<>(input);
-        Integer expected = 4;
-        Integer actual = mySet.getMySet().size();
+        int expected = 4;
+        int actual = mySet.size();
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void constructor2Test(){
+        MySet<Integer> mySet = new MySet<>();
+        int expected = 16;
+        int actual = mySet.size();
+        Assert.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void constructor3Test(){
+        MySet<Integer> mySet = new MySet<>(3);
+        int expected = 3;
+        int actual = mySet.size();
         Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void addTest(){
-        Integer[] input = {1,2,3,4,5};
+        Integer[] input = {1,2,3,4,6};
         MySet<Integer> mySet = new MySet<>(input);
         Integer toBeAdded = 6;
-        Object[] expected = {1,2,3,4,5,6};
+        Object[] expected = {1,2,3,4,6};
         mySet.add(toBeAdded);
-        Object[] actual = mySet.getMySet().getMyArrayList();
+        Object[] actual = mySet.getMySet();
+        Assert.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void removeTest(){
+        Integer[] input = {1,2,3,4,5};
+        MySet<Integer> mySet = new MySet<>(input);
+        int index = 2;
+        Object[] expected = {1,2,4,5};
+        mySet.remove(index);
+        Object[] actual = mySet.getMySet();
+        Assert.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void sizeTest(){
+        Integer[] input = {1,2,3,4,5};
+        MySet<Integer> mySet = new MySet<>(input);
+        int expected = 5;
+        int actual = mySet.size();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void containsTest(){
+        Integer[] input = {1,2,3,4,5};
+        MySet<Integer> mySet = new MySet<>(input);
+        boolean expected = true;
+        boolean actual = mySet.contains(3);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void isEmptyTest(){
+        Integer[] input = {4};
+        MySet<Integer> mySet = new MySet<>(input);
+        boolean expected = false;
+        boolean actual = mySet.isEmpty();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void isEmpty2Test(){
+        Integer[] input = {};
+        MySet<Integer> mySet = new MySet<>(input);
+        boolean expected = true;
+        boolean actual = mySet.isEmpty();
+        Assert.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void clearTest(){
+        Integer[] input = {1,2,3,4,5};
+        MySet<Integer> mySet = new MySet<>(input);
+        Object[] expected = {};
+        mySet.clear();
+        Object[] actual = mySet.getMySet();
         Assert.assertArrayEquals(expected, actual);
     }
 
