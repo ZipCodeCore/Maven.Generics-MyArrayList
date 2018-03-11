57734ae078e2f3d73b28c99cc45a9c6ab8fdf7f5
Eric Cordell <ericcordell@zipcoders-MacBook-Pro-8.local>
2018-03-11 17:20:24 -0400

    stubbing out part 2 methods

---
+++ src/main/java/MySet.java
@@ -0,0 +1,44 @@
+public class MySet <E> {
+
+    MyArrayList<E> mySet;
+
+    public MySet() {
+        mySet = new MyArrayList<>();
+    }
+
+    public MySet(int size) {
+
+    }
+
+    public void add(E elementToAppend) {
+
+    }
+
+    public void addAtIndex(Integer index, E elementToInsert) {
+
+    }
+
+    public E get(Integer index) {
+        return null;
+    }
+
+    public void remove(Integer index) {
+
+    }
+
+    public E set(Integer index, E elementToStore) {
+        return null;
+    }
+
+    public void clear() {
+
+    }
+
+    public boolean isEmpty() {
+        return false;
+    }
+
+    public boolean contains(E value) {
+        return false;
+    }
+}
