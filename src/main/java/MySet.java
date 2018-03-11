723b9c3864a899379cef6c436eb23480ab402e58
Joshua Wurdemann <joshuawurdemann@zipcoders-MacBook-Pro-9.local>
2018-03-11 11:44:36 -0400

    updated set add

--- src/main/java/MySet.java
+++ src/main/java/MySet.java
@@ -1,28 +1,35 @@
+import com.sun.tools.classfile.Opcode;
+
 import java.util.Set;
 
 public class MySet<E> extends MyCollections<E> {
+    public MySet() {
+        super(DEFAULT_SIZE);
+    }
 
-   public MySet(){
-    super(DEFAULT_SIZE);
-   }
-
-   @Override
-   public boolean add(E theObject){
-
-      return false;
-   }
-/*It is reflexive: for any non-null reference value x, x.equals(x) should return true.
-It is symmetric: for any non-null reference values x and y, x.equals(y) should
- return true if and only if y.equals(x) returns true.
-It is transitive: for any non-null reference values x, y, and z, if x.equals(y)
-returns true and y.equals(z) returns true, then x.equals(z) should return true.
-It is consistent: for any non-null reference values x and y, multiple invocations of
-x.equals(y) consistently return true or consistently return false, provided no
-information used in equals comparisons on the objects is modified.*/
+    @Override
+    public boolean add(E theObject) {
+        boolean added = false;
+        if (!contains(theObject))
+            super.add(theObject);
+        added = true;
+        return added;
+    }
 
 
-   @Override
-   public boolean equals(Object theObject) {
-      return false;
-   }
+    @Override
+    public boolean equals(Object theObject) {
+        //check to see if TheObject is an instance of the set
+        // check to see if sizes are the same
+        // check to see if the one set contains all the same object
+        if (!(theObject instanceof MySet)) {
+            return false;
+        }
+        if ((((MySet) theObject).getSize() != getSize()))
+            if (!((MySet) theObject).contains(theObject)) {
+                return false;
+            }
+
+        return theObject.equals(getNumbers());
+    }
 }
